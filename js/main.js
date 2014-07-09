aa1bcc24d7e3dd0a223403dda617ae03922d74bd
Vicky <viikadrug@Vickys-MacBook-Pro.local>
2014-07-09 21:28:17 +0400

    added nav & slider blocks

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@
 
 $(document).on('ready', function(){
     $('.no-placeholder input[placeholder], .no-placeholder textarea[placeholder]').placeholder();
+
+    $('.phones__handler').on('click',function(){
+        $('.phones__list').fadeToggle();
+    });
+
+    $(".nav__item").on('mouseover', function(){
+        $(this).children('.nav__item__pin').css('display','block');
+    });
+    $(".nav__item").on('mouseout', function(){
+        $(this).children('.nav__item__pin').css('display','none');
+    });
+
 });
